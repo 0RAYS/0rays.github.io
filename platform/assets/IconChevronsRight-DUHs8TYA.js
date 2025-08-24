94f509d1116bb5b1da59535de9f094c8d72abcc5
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-08-24 18:13:19 +0800

    update

---
+++ platform/assets/IconChevronsRight-DUHs8TYA.js
@@ -0,0 +1,11 @@
+import{c as o}from"./index-DZsURnR3.js";/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M11 7l-5 5l5 5",key:"svg-0"}],["path",{d:"M17 7l-5 5l5 5",key:"svg-1"}]],s=o("outline","chevrons-left","ChevronsLeft",e);/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M7 7l5 5l-5 5",key:"svg-0"}],["path",{d:"M13 7l5 5l-5 5",key:"svg-1"}]],c=o("outline","chevrons-right","ChevronsRight",t);export{s as I,c as a};
