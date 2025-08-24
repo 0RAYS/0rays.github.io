efc9a04959cb7010d8b126a07ab995cde6421680
JBNRZ <76835191+JBNRZ@users.noreply.github.com>
2025-08-24 13:54:39 +0800

    update

---
+++ platform/assets/IconHistory-M5_FcCuI.js
@@ -0,0 +1,11 @@
+import{c as o}from"./index-Db61s3TU.js";/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0",key:"svg-0"}],["path",{d:"M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6",key:"svg-1"}]],s=o("outline","eye","Eye",t);/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M12 8l0 4l2 2",key:"svg-0"}],["path",{d:"M3.05 11a9 9 0 1 1 .5 4m-.5 5v-5h5",key:"svg-1"}]],a=o("outline","history","History",e);export{a as I,s as a};
